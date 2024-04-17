4bc24b3b7c885fc5c4dead5293efac5bc171e915
DevzekiFaith <zekipossible@gmail.com>
2024-04-17 06:55:35 +0100

    ninth Commit

--- src/Component/NotFoundPage/NotFoundPage.jsx
+++ src/Component/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SearchImage from "/public/SearchImage.jpg.svg"
+import SearchImage from "/SearchImage.jpg.svg"
 import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
